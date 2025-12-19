7fba08e9e8370083355328c4a3dc4f223bb9992d
Titouan G <titouan.garnier7@gmail.com>
2025-12-19 09:19:31 +0100

    Add files via upload
    ajouts des commentaires

--- PJsite1/collection.js
+++ PJsite1/collection.js
@@ -1,17 +1,17 @@
-darkModeBtn = document.getElementById("darkModeBtn")
+darkModeBtn = document.getElementById("darkModeBtn") // selectionne le bouton
 
 elements = document.querySelectorAll(
   "body, a, header, .case, .cocktails, .footer, #darkModeBtn"
-)
+) // selectionne tous ce qu'il faut passer en dark mode 
 
 darkModeBtn.addEventListener("click", function() {
   elements.forEach(function(element){
     element.classList.toggle("dark-mode")
-  })
+  }) // active ou desactive le dark mode pour les elements selectionnés
   if (document.body.classList.contains("dark-mode")) {
     darkModeBtn.textContent = "Light Mode"
   } else {
     darkModeBtn.textContent = "Dark Mode"
   }
-})
+}) // change le texte du bouton selon le mode actuel 
 
--- PJsite1/main.js
+++ PJsite1/main.js
@@ -1,9 +1,8 @@
-menuBurger = document.querySelector('.menu-burger')
+menuBurger = document.querySelector('.menu-burger') // selectionne le menu burger
 
-menuCache = document.querySelector('.menu-cache')
+menuCache = document.querySelector('.menu-cache')// selectionne le menu caché 
 
 menuBurger.addEventListener('click', function() {
     menuBurger.classList.toggle('visible')
     menuCache.classList.toggle('visible')
-
-})
+}) // active les elements en visible 
